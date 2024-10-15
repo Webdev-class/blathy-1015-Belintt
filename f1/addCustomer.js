158b31a84be361a2f78faf73ba7b52d7e9d3d9f4
Belintt <kovacshoffman.balint@blathy.info>
2024-10-15 13:51:51 +0200

    f1_test

--- f1/addCustomer.js
+++ f1/addCustomer.js
@@ -12,14 +12,31 @@ let customers = [
 ]
 
 function isAddress(address) {
-    //TODO
+    return typeof address.city === 'string' &&
+           typeof address.street === 'string' &&
+           typeof address.house === 'number';
 }
 
 function isCustomer(name, age, address, newsLetter) {
-    //TODO
+    return typeof name === 'string' &&
+           typeof age === 'number' &&
+           isAddress(address) &&
+           typeof newsLetter === 'boolean';
 }
 
 function addCustomer(name, age, address, newsLetter) {
-    //TODO
+    if (isCustomer(name, age, address, newsLetter)) {
+        const newCustomer = {
+            id: customers.length + 1,
+            name,
+            age,
+            address,
+            newsLetter
+        };
+        customers.push(newCustomer);
+        return newCustomer;
+    } else {
+        throw new Error("Invalid customer data.");
+    }
 }
 module.exports = addCustomer;
